fc253ed69ae6ed817551b0e5692e598360b8c9e9
curiousdragon <jamie4hong7@gmail.com>
2018-04-13 21:50:05 -0700

    blankIndexes, blankAnswer, blankNumber added

--- Essay.js
+++ Essay.js
@@ -22,6 +22,7 @@ export default class Essay extends React.Component {
   getInitialState() {
     return {
       blankIndex: 0,
+      blankAnswer: '',
     }
   }
 
@@ -31,25 +32,28 @@ export default class Essay extends React.Component {
 
         {this.renderTemplate()}
 
-        <View style={styles.containerButton}>
-          <Button title='I am done with my essay'
-            onPress={this._handlePress.bind(this)} 
-            style={styles.button}>
-          </Button>
-        </View>
-
+        <View style={styles.container}>
         <Text>Word Bank</Text>
         <Text>Blank number: {this.state.blankIndex}</Text>
+        <Text>Selected word: {this.state.blankAnswer}</Text>
         <Picker
-        selectedValue={this.words[0]}
+        selectedValue={this.state.blankAnswer}
         style={{ height: 50, width: 200 }}
-        onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+        onValueChange={(itemValue, itemIndex) => this.setState({blankAnswer: itemValue})}>
         
         {this.words.map((word) => {
              return <Picker.Item key={word.str} label={word.str} value={word.str} />
           })}
 
         </Picker>
+        </View>
+
+        <View style={styles.containerButton}>
+          <Button title='I am done with my essay'
+            onPress={this._handlePress.bind(this)} 
+            style={styles.button}>
+          </Button>
+        </View>
 			</View>
       );
 	}
@@ -64,7 +68,7 @@ export default class Essay extends React.Component {
         {this.template.lines.map((line, i) => {
           return (
               <View key={i}>
-              <Text>{line}</Text>
+              <Text style={{fontSize: 18}}>{line}</Text>
               <Button title='_blank_'
                 onPress={() => this.setState({blankIndex: this.template.lineIndexes[line]})}
                 style={styles.containerButton}>
@@ -115,8 +119,6 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.blue,
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 20,
-    paddingLeft: 40,
-    paddingRight: 40,
+    margin: 20, 
   },
 });
